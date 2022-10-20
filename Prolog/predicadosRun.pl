315921c2952fe7d7496bd93e3d105929e5189df0
Bruno_Calisto <1181426@isep.ipp.pt>
2022-10-20 17:15:11 +0100

    More predicates for Prolog

--- Prolog/predicadosRun.pl
+++ Prolog/predicadosRun.pl
@@ -19,16 +19,38 @@ energyCert(id,perc).
 
 postalCode(pref,sufix).
 
+condition("Normal",0).
+condition("New",0.05).
+condition("Renovated",0.02).
+condition("Old",-0.05).
+
 estate(1,"Apartment","New",117,"T3",1970,"A2",0,2,"Rua X",[4400,130],15000,["Pool","Terrace"]).
 estate(2,"Apartment","New",123,"T3",1978,"A2",1,2,"Rua T",[4400,121],15000,[]).
 estate(3,"Apartment","New",103,"T3",1974,"A2",3,2,"Rua Y",[4400,130],15000,[]).
 estate(4,"Apartment","New",133,"T3",1972,"A2",2,2,"Rua U",[4400,133],15000,[]).
 estate(5,"Apartment","New",124,"T3",1979,"A2",1,2,"Rua K",[4400,140],15000,[]).
 
+evaluation(1,1).
+
+/*FALTA FAZER*/
+gera_facto:.
+
+evaluateComponent(Value,Desc,FinalValue,Element,Mode):-    
+    (Element == "Condition" ->
+        condition(Desc,perc),
+        (Mode == "Appreciate" ->    
+            Multiplier is 1+perc;
+            Multiplier is 1-perc);
+        energyCert(Desc,perc)        
+        (Mode == "Appreciate" ->    
+            Multiplier is 1+RoomValue;
+            Multiplier is 1-RoomValue)),    
+    FinalValue is Multiplier*Value.
+
 evaluateConstYear(Value,ConstYear,FinalValue,Mode):-
     currYear(CurrYear),
     depByYear(DepYear),
-    (Mode == Appreciate ->
+    (Mode == "Appreciate" ->
         Multiplier is 1-DepYear;
         Multiplier is 1+DepYear),
     DiffYears is CurrYear-ConstYear,
@@ -36,18 +58,19 @@ evaluateConstYear(Value,ConstYear,FinalValue,Mode):-
     FinalValue is Res*Value.
 
 evaluateSpots(Value,Number,FinalValue,Element,Mode):-
-    aprecByParkSlot(ParkValue),
-    aprecByBathRoom(RoomValue),
     (Element == "Park" ->
+        aprecByParkSlot(ParkValue),
         (Mode == "Appreciate" ->    
             Multiplier is 1+ParkValue;
-            Multiplier is 1-ParkValue);        
+            Multiplier is 1-ParkValue);
+        aprecByBathRoom(RoomValue),
         (Mode == "Appreciate" ->    
             Multiplier is 1+RoomValue;
             Multiplier is 1-RoomValue)),    
     potencia(Multiplier,Number,Res),
     FinalValue is Res*Value.
 
+/*FALTA FAZER*/
 evaluateExtras(Value,Extras,FinalValue).
 
 potencia(X,1,X).
@@ -57,12 +80,14 @@ potencia(Base,Potencia,Res):-
     Res is Res2 * Base.
 
 
-evaluate(estate,BaseValue, "appreciate").
-evaluate(estate,BaseValue, "depreciate"):-
-
-
-
-evaluation(1,1).
+evaluate(Estate,BaseValue,Mode,FinalValue):-
+    estate(Estate,_,Condition,_,_,ConstYear,EnergyCert,ParkSlots,RoomValue,_,_,Value,ListaExtras),
+    evaluateConstYear(Value,ConstYear,Value1,Mode),
+    evaluateSpots(Value1,ParkSlots,Value2,"Park",Mode),
+    evaluateSpots(Value2,RoomValue,Value3,"NBath",Mode),
+    evaluateComponent(Value3,Condition,Value4,"Condition",Mode),    
+    evaluateComponent(Value4,EnergyCert,Value5,"EnergyCert",Mode),
+    evaluateExtras(Value5,ListaExtras,FinalValue,Mode).
 
 get_average(List,Average):-
     sum( List, Sum ),
