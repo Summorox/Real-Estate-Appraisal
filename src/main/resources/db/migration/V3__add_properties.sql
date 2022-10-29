bf970f4f185aaabd7633a32a2143536200c4d1a8
alessandro.cunha <1220183@isep.ipp.pt>
2022-10-29 18:24:32 +0100

    Add script to insert sample property for T1 in Paranhos. #U3gLNb3p

---
+++ src/main/resources/db/migration/V3__add_properties.sql
@@ -0,0 +1,9 @@
+insert into postal_code(id, prefix_code, suffix_code) values (1, 4200, 000);
+
+
+insert into property(id, price, typology, postal_code_id) values (1, 10500, 1, 1);
+insert into property(id, price, typology, postal_code_id) values (2,130000, 1, 1);
+insert into property(id, price, typology, postal_code_id) values (3,145000, 1, 1);
+insert into property(id, price, typology, postal_code_id) values (4,243486, 1, 1);
+insert into property(id, price, typology, postal_code_id) values (5,225000, 1, 1);
+insert into property(id, price, typology, postal_code_id) values (6,180000, 1, 1);
