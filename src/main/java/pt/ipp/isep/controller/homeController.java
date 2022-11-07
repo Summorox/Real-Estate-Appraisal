564c140429a7cb4ad5c97df4f4331902934024ef
alessandro.cunha <1220183@isep.ipp.pt>
2022-11-07 17:53:46 +0000

    Create a frontend

---
+++ src/main/java/pt/ipp/isep/controller/homeController.java
@@ -0,0 +1,14 @@
+package pt.ipp.isep.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        return "home2";
+    }
+}
